fcd6d6c5efef364381903563e11ca7b9b0a31af5
rubaiiyat <abirr1516@gmail.com>
2025-05-22 22:22:10 +0600

    Item added from form completed

--- Coffee-Store-React/src/Components/AddCoffee.jsx
+++ Coffee-Store-React/src/Components/AddCoffee.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Swal from "sweetalert2";
 
 const AddCoffee = () => {
   const handleAddCoffee = (event) => {
@@ -13,25 +14,58 @@ const AddCoffee = () => {
     const category = form.category.value;
     const details = form.details.value;
     const photo = form.photo.value;
+    const price = form.price.value;
 
-    const coffee = { name, chef, supplier, taste, category, details, photo };
+    const coffee = {
+      name,
+      chef,
+      supplier,
+      taste,
+      category,
+      details,
+      photo,
+      price,
+    };
     console.log(coffee);
+
+    fetch("http://localhost:3000/coffee", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(coffee),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+
+        if (data.insertedId) {
+          Swal.fire({
+            title: "Success!",
+            text: "Item Added Successfully",
+            icon: "success",
+            draggable: true,
+          });
+        }
+      });
   };
+
   return (
     <div>
       <form
         onSubmit={handleAddCoffee}
-        className="max-w-4xl mx-auto p-5 md:p-10 bg-base-200 shadow-md rounded-xl "
+        className="max-w-4xl mx-auto p-5 md:p-10 bg-base-200 shadow-md rounded-xl"
       >
         <h1 className="text-3xl font-bold text-center mb-8 text-[#d2b48c] font-mono">
           Add New Coffee
         </h1>
         <p className="text-sm mb-10 font-mono">
-          It is a long established fact that a reader will be distraceted by the
+          It is a long established fact that a reader will be distracted by the
           readable content of a page when looking at its layout. The point of
           using Lorem Ipsum is that it has a more-or-less normal distribution of
           letters, as opposed to using Content here.
         </p>
+
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           {/* Name */}
           <div className="relative z-0 w-full group">
@@ -45,9 +79,7 @@ const AddCoffee = () => {
             />
             <label
               htmlFor="name"
-              className="absolute text-sm text-gray-300 duration-300 transform -translate-y-6 scale-75 top-3 left-0 -z-10 origin-left
-                peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100
-                peer-focus:-translate-y-6 peer-focus:scale-75"
+              className="absolute text-sm text-gray-300 duration-300 transform -translate-y-6 scale-75 top-3 left-0 -z-10 origin-left peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:-translate-y-6 peer-focus:scale-75"
             >
               Name
             </label>
@@ -65,9 +97,7 @@ const AddCoffee = () => {
             />
             <label
               htmlFor="chef"
-              className="absolute text-sm text-gray-300 duration-300 transform -translate-y-6 scale-75 top-3 left-0 -z-10 origin-left
-                peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100
-                peer-focus:-translate-y-6 peer-focus:scale-75"
+              className="absolute text-sm text-gray-300 duration-300 transform -translate-y-6 scale-75 top-3 left-0 -z-10 origin-left peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:-translate-y-6 peer-focus:scale-75"
             >
               Chef
             </label>
@@ -85,9 +115,7 @@ const AddCoffee = () => {
             />
             <label
               htmlFor="supplier"
-              className="absolute text-sm text-gray-300 duration-300 transform -translate-y-6 scale-75 top-3 left-0 -z-10 origin-left
-                peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100
-                peer-focus:-translate-y-6 peer-focus:scale-75"
+              className="absolute text-sm text-gray-300 duration-300 transform -translate-y-6 scale-75 top-3 left-0 -z-10 origin-left peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:-translate-y-6 peer-focus:scale-75"
             >
               Supplier
             </label>
@@ -105,9 +133,7 @@ const AddCoffee = () => {
             />
             <label
               htmlFor="taste"
-              className="absolute text-sm text-gray-300 duration-300 transform -translate-y-6 scale-75 top-3 left-0 -z-10 origin-left
-                peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100
-                peer-focus:-translate-y-6 peer-focus:scale-75"
+              className="absolute text-sm text-gray-300 duration-300 transform -translate-y-6 scale-75 top-3 left-0 -z-10 origin-left peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:-translate-y-6 peer-focus:scale-75"
             >
               Taste
             </label>
@@ -125,9 +151,7 @@ const AddCoffee = () => {
             />
             <label
               htmlFor="category"
-              className="absolute text-sm text-gray-300 duration-300 transform -translate-y-6 scale-75 top-3 left-0 -z-10 origin-left
-                peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100
-                peer-focus:-translate-y-6 peer-focus:scale-75"
+              className="absolute text-sm text-gray-300 duration-300 transform -translate-y-6 scale-75 top-3 left-0 -z-10 origin-left peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:-translate-y-6 peer-focus:scale-75"
             >
               Category
             </label>
@@ -145,33 +169,47 @@ const AddCoffee = () => {
             />
             <label
               htmlFor="details"
-              className="absolute text-sm text-gray-300 duration-300 transform -translate-y-6 scale-75 top-3 left-0 -z-10 origin-left
-                peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100
-                peer-focus:-translate-y-6 peer-focus:scale-75"
+              className="absolute text-sm text-gray-300 duration-300 transform -translate-y-6 scale-75 top-3 left-0 -z-10 origin-left peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:-translate-y-6 peer-focus:scale-75"
             >
               Details
             </label>
           </div>
-        </div>
 
-        {/* Photo Upload */}
-        <div className="relative z-0 w-full mt-6 group">
-          <input
-            type="text"
-            name="photo"
-            id="photo"
-            placeholder=" "
-            required
-            className="block py-2.5 px-0 w-full text-white bg-transparent border-0 border-b-2 border-gray-500 appearance-none focus:outline-none focus:ring-0 focus:border-[#D2B48C] peer"
-          />
-          <label
-            htmlFor="photo"
-            className="absolute text-sm text-gray-300 duration-300 transform -translate-y-6 scale-75 top-3 left-0 -z-10 origin-left
-              peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100
-              peer-focus:-translate-y-6 peer-focus:scale-75"
-          >
-            Photo URL
-          </label>
+          {/* Photo */}
+          <div className="relative z-0 w-full group">
+            <input
+              type="text"
+              name="photo"
+              id="photo"
+              placeholder=" "
+              required
+              className="block py-2.5 px-0 w-full text-white bg-transparent border-0 border-b-2 border-gray-500 appearance-none focus:outline-none focus:ring-0 focus:border-[#D2B48C] peer"
+            />
+            <label
+              htmlFor="photo"
+              className="absolute text-sm text-gray-300 duration-300 transform -translate-y-6 scale-75 top-3 left-0 -z-10 origin-left peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:-translate-y-6 peer-focus:scale-75"
+            >
+              Photo URL
+            </label>
+          </div>
+
+          {/* Price */}
+          <div className="relative z-0 w-full group">
+            <input
+              type="number"
+              name="price"
+              id="price"
+              placeholder=" "
+              required
+              className="block py-2.5 px-0 w-full text-white bg-transparent border-0 border-b-2 border-gray-500 appearance-none focus:outline-none focus:ring-0 focus:border-[#D2B48C] peer"
+            />
+            <label
+              htmlFor="price"
+              className="absolute text-sm text-gray-300 duration-300 transform -translate-y-6 scale-75 top-3 left-0 -z-10 origin-left peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:-translate-y-6 peer-focus:scale-75"
+            >
+              Price (USD)
+            </label>
+          </div>
         </div>
 
         {/* Submit */}
